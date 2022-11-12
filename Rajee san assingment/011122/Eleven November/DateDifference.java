8acec85ddb1fea7f50bc57c6a57c0d752c394d84
Junai <96984477+Junai13@users.noreply.github.com>
2022-11-12 12:22:18 +0530

    Add files via upload

--- Rajee san assingment/011122/Eleven November/DateDifference.java
+++ Rajee san assingment/011122/Eleven November/DateDifference.java
@@ -11,26 +11,26 @@ public class DateDifference {
         dateDifference.getInput();
     }
     private void getInput(){
-        String d1 = inputValidator("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$");
-        String d2 = inputValidator("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$");
+        String d1 = inputValidator();
+        String d2 = inputValidator();
         String[] temp1= d1.split("/");
         String[] temp2= d2.split("/");
         Date date1 = new Date(Integer.parseInt(temp1[0]),Integer.parseInt(temp1[1]),Integer.parseInt(temp1[2]));
         Date date2 = new Date(Integer.parseInt(temp2[0]),Integer.parseInt(temp2[1]),Integer.parseInt(temp2[2]));
-        System.out.println("\n\nDifference between two dates are " +getDifference(date1,date2) );
+        System.out.println("\n\nDifference between two dates : " +getDifference(date1,date2) );
     }
-    private String inputValidator(String regex) {
+    private String inputValidator() {
+        Pattern reg = Pattern.compile("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$");
         Scanner sc = new Scanner(System.in);
         String input;
         System.out.print("\nEnter Date : ");
         input = sc.nextLine();
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
+        Matcher matcher = reg.matcher(input);
         if (matcher.matches())
             return input;
         else {
             System.out.println("Invalid input or invalid format of input!!");
-            return inputValidator(regex);
+            return inputValidator();
         }
     }
     private int getDifference(Date date1, Date date2)
